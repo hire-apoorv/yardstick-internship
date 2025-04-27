386e2925e3c3908ae3b9118a14e932bf1af2ca2c
apoorv jain <apoorvjain@apoorvs-MacBook-Air.local>
2025-04-27 14:10:09 +0530

    Stage 1.1

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,19 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAUk_XnVKYWO3RfSR77nFb7GWt7qDAoTqo",
-  authDomain: "yardstick-internship-62b54.firebaseapp.com",
-  projectId: "yardstick-internship-62b54",
-  storageBucket: "yardstick-internship-62b54.firebasestorage.app",
-  messagingSenderId: "883404550895",
-  appId: "1:883404550895:web:b91834a6d9e8b280a5211a"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export default app;
